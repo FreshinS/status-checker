d38aea9f4c3ff6866c0fb19c08f19fbcb679deb3
Ilya <GromovIN@navigat.ru>
2025-08-01 11:38:47 +0300

    feat: Изменён запрос в бд

--- db.js
+++ db.js
@@ -1,18 +1,5 @@
 require('dotenv').config();
-// const sql = require('mssql');
 const odbc = require('odbc');
-
-// const config = {
-//   user: process.env.MSSQL_USER,
-//   password: process.env.MSSQL_PASS,
-//   server: process.env.MSSQL_SERVER,
-//   database: process.env.MSSQL_DATABASE,
-//   port: parseInt(process.env.MSSQL_PORT),
-//   options: {
-//     encrypt: false,
-//     trustServerCertificate: true,
-//   },
-// };
 
 let pool;
 
@@ -25,8 +12,40 @@ async function getConnection() {
 
 async function getEmployees() {
   const conn = await getConnection();
-  const result = await conn.query(`SELECT id, name, is_present FROM Employees`);
-  return result;
+  const result = await conn.query(`
+    SELECT
+      plog.HozOrgan AS id,
+      employees.TabNumber AS tab_number,
+      employees.Name + ' ' + employees.FirstName + ' ' + employees.MidName AS full_name,
+      MAX(plog.TimeVal) AS last_time,
+      plog.Mode AS mode
+    FROM
+      OrionNavigat.dbo.PLogData AS plog
+    LEFT JOIN
+      OrionNavigat.dbo.PList AS employees
+      ON plog.HozOrgan = employees.ID
+    WHERE
+      plog.doorIndex IN (1, 2, 10, 14, 16, 18, 19, 20, 30, 31, 32, 34, 35, 36, 42, 45, 48, 49, 52)
+      AND plog.TimeVal > CAST(GETDATE() AS date)
+      AND plog.HozOrgan <> 0
+      AND employees.TabNumber <> ''
+      AND employees.Section <> 62
+      AND plog.Mode = 1
+      AND plog.Event = 32
+    GROUP BY
+      employees.TabNumber,
+      plog.HozOrgan,
+      employees.Name + ' ' + employees.FirstName + ' ' + employees.MidName,
+      plog.Mode
+  `);
+
+  // Формируем структуру с is_present = true (так как Mode=1 в WHERE)
+  return result.map(emp => ({
+    id: emp.id,
+    name: emp.full_name,
+    is_present: true, // можно дополнить позже Mode'ом, если нужно оба направления
+    time: emp.last_time
+  }));
 }
 
 module.exports = { getEmployees };
